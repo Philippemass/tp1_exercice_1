346ff05cd920cb3ea859effdcdec8141db42586d
Philippemass <philippe.massaad.1@ulaval.ca>
2013-02-25 20:05:46 -0500

    Update exercice_1_1.dart

--- exercice_1_1.dart
+++ exercice_1_1.dart
@@ -1,5 +1,6 @@
 library exercise;
 import 'dart:math';
+
 part 'fonctions.dart';
 
 
@@ -26,22 +27,29 @@ ex1_2() {
      resultat = multiplication(multiplicateur, i);
      print(resultat);
   }
+  print(''); // sert d'espacement entre les 2 exercices
 }
 
 ex1_3() {
-  
+  print ('Exercice 1.3');
+ 
 }
 
 ex1_4(){
-  
+  print ('Exercice 1.4');
+  var radian = 6.28318531; // représente 360 degrés
+  var degres = (radian * diviser(180, PI)).toInt();
+  var minutes = diviser(degres, 6).toInt(); // 360 degrés = 60 minutes donc 1 minute = 6 degrés
+  var secondes = minutes*60;
+  print('${radian} Radians équivaut à ${degres} Degrés, ${minutes} Minutes, ${secondes} Secondes');
 }
-
+ex1_5(){
+ print ('Exercice 1.5'); 
+}
 main () {
   ex1_1();
   ex1_2();
   ex1_3();
   ex1_4();
+  ex1_5();
 }
-
-
-
